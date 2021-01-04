de998b31210b50b5ea4f2978db9d0f70d1d6ccb6
kiranmanojkumar <manojkiran80@gmail.com>
2021-01-03 19:10:12 -0500

    added url copy button

--- src/components/SocialShare.jsx
+++ src/components/SocialShare.jsx
@@ -12,50 +12,102 @@ import {
     LinkedinIcon,
     WhatsappShareButton,
     WhatsappIcon,
-    
-  } from "react-share";
+
+} from "react-share";
+import FileCopyIcon from '@material-ui/icons/FileCopy';
+import Snackbar from '@material-ui/core/Snackbar';
+import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from '@material-ui/icons/Close';
+import copy from 'copy-to-clipboard';
+import FileCopyOutlinedIcon from '@material-ui/icons/FileCopyOutlined';
 class SocialShare extends React.Component {
-    constructor(props){
+    constructor(props) {
         super(props);
+        this.state = {
+            showCopiedMessage: false
+        }
     }
 
-    render(){
-        return(
-            <>
+    handleClose = () => {
+        this.setState({
+            showCopiedMessage: false
+        })
+    }
+
+    handleURLCopy = ()=>{
+        if(copy(this.props.url)){
+            this.setState({
+                showCopiedMessage: true
+            })
+        }
+    }
+
+    render() {
+        return (
+            <div className='socials-container'>
                 <div className='socials-row'>
                     <div className='social-icon'>
-                        <EmailShareButton 
+                    <IconButton onClick={this.handleURLCopy}>
+                            <FileCopyOutlinedIcon />
+                        </IconButton>
+                    </div>
+                    <div className='social-icon'>
+                        <EmailShareButton
                             subject="Take a look at Kiran's website"
-                            children={<EmailIcon size={32} round={true}/>} 
+                            children={<EmailIcon size={32} round={true} />}
                             url={this.props.url}
                         />
                     </div>
                     <div className='social-icon'>
-                        <FacebookShareButton 
-                            children={<FacebookIcon size={32} round={true}/>} 
+                        <FacebookShareButton
+                            children={<FacebookIcon size={32} round={true} />}
                             url={this.props.url}
                         />
                     </div>
                     <div className='social-icon'>
-                        <TwitterShareButton 
-                            children={<TwitterIcon size={32} round={true}/>} 
+                        <TwitterShareButton
+                            children={<TwitterIcon size={32} round={true} />}
                             url={this.props.url}
                         />
                     </div>
                     <div className='social-icon'>
-                        <LinkedinShareButton 
-                            children={<LinkedinIcon size={32} round={true}/>} 
+                        <LinkedinShareButton
+                            children={<LinkedinIcon size={32} round={true} />}
                             url={this.props.url}
                         />
                     </div>
                     <div className='social-icon'>
-                        <WhatsappShareButton 
-                            children={<WhatsappIcon size={32} round={true}/>} 
+                        <WhatsappShareButton
+                            children={<WhatsappIcon size={32} round={true} />}
                             url={this.props.url}
                         />
                     </div>
                 </div>
-            </>
+
+                <div>
+                    Share this page!
+                </div>
+
+
+
+                <Snackbar
+                    anchorOrigin={{
+                        vertical: 'bottom',
+                        horizontal: 'center',
+                    }}
+                    open={this.state.showCopiedMessage}
+                    autoHideDuration={3000}
+                    onClose={this.handleClose}
+                    message="URL copied!"
+                    action={
+                        <React.Fragment>
+                            <IconButton size="small" aria-label="close" color="inherit" onClick={this.handleClose}>
+                                <CloseIcon fontSize="small" />
+                            </IconButton>
+                        </React.Fragment>
+                    }
+                />
+            </div>
         )
     }
 }
