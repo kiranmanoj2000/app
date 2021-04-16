c3d9ef2abdc3c5cc15a9072a31db5c83e645a48f
kiranmanojkumar <manojkiran80@gmail.com>
2021-04-15 21:14:24 -0400

    removed testing code

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import PageHeading from './PageHeading'
 import {HOMEPAGE_FIRST_PARAGRAPH} from '../constants/Home/index'
+import Cookies from 'universal-cookie';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button'
+const cookies = new Cookies();
 class HomePage extends React.Component {
+    
+    
+
     constructor(props){
         super(props)
-        
+        this.state = {
+            cookieValue: cookies.get('cookie') ? cookies.get('cookie') : ""
+        }
+    }
+
+    saveCookie = () => {
+        cookies.set('cookie', this.state.cookieValue, { path: '/' });
     }
 
   
@@ -13,6 +26,13 @@ class HomePage extends React.Component {
             <> 
                 
                     <PageHeading heading='Home' description={HOMEPAGE_FIRST_PARAGRAPH}/>
+
+        {/* <TextField id="filled-basic" value={this.state.cookieValue} onChange={(e)=>{this.setState({cookieValue: e.target.value})}} label="Value" variant="filled"/>
+        <Button variant="contained" onClick={this.saveCookie} color="primary" >
+            Save
+        </Button> */}
+
+                    
             </>
         )
     }
